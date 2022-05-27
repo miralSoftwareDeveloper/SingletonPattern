727a91ad5206bdf88cacc2c816656ed06830c775
Miraj Shah <Miraj Shah@DESKTOP-633EA8H>
2022-05-27 13:35:54 +0530

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace SingletonPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SingletonVersion1 firstObject = SingletonVersion1.GetInstance;
+            Console.ForegroundColor = ConsoleColor.White;
+            firstObject.PrintDetails("Created first instance from one object.");
+            SingletonVersion1 secondObject = SingletonVersion1.GetInstance;
+            Console.ForegroundColor = ConsoleColor.Red;
+            secondObject.PrintDetails("Creating second instance by creating second object.");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Ouput clearly shows that the private constructor is executed once.");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Even though constructor is called by two different objects.");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("This version is not thread safe.");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("If both threads get instance null, private constructor will called twice.");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Real Time Example :Service Proxies,Database Connection Class,Logger,");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ SingletonVersion1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonPattern
+{
+    //First version - not thread-safe
+    public sealed class SingletonVersion1
+    {
+        private static int counter = 0;
+        private static SingletonVersion1 instance=null;
+        
+        // Private Constructor 
+        private SingletonVersion1()
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            counter++;
+            Console.WriteLine("Private Constructor Called Counter " + counter.ToString());
+        }
+
+        public static SingletonVersion1 GetInstance
+        {
+            get
+            {
+                if (instance == null)
+                { 
+                  instance = new SingletonVersion1();
+                }
+                return instance;
+            }
+        }
+
+        public void PrintDetails(string message)
+        {
+           
+            Console.WriteLine(message);
+        }
+    }
+}
